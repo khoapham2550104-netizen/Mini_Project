70345417fc9d7b1a6980722fa55f6351b041cb14
NoobieK <khoa.pham2550104@hcmut.edu.vn>
2026-07-18 18:22:28 +0700

    Base Design

---
+++ Tick-Tac-Toe/python_code/tick_tac_toe.py
@@ -0,0 +1,12 @@
+class TickTacToe:
+    
+    grid = [[] * 3 for i in range(3)]
+    def __init__(self):
+        pass
+    
+    def putX(self):
+        pass
+
+    def putY(self):
+        pass
+        
