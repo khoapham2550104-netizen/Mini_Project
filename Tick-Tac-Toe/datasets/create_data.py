8621067d8d0cbf3fa06eb4ca7c45c077c415ef69
NoobieK <khoa.pham2550104@hcmut.edu.vn>
2026-07-28 20:23:53 +0700

    Delete more

--- Tick-Tac-Toe/datasets/create_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from itertools import product
-
-values = (0, 1, -1)
-df  = pd.DataFrame(product(values, repeat= 9))
-df.columns = [f"{i}" for i in range(9)]
-df.to_csv("all_combination.csv", index= False)
